6df7d554b1a5033a7cc2d6e3bcb428a0de4716e0
fallred <zhangqiuhong@baijiahualian.com>
2023-05-25 16:47:51 +0800

    feat: fix pro

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,29 +1,30 @@
 // https://github.com/arackaf/customize-cra/blob/master/api.md
 const path = require('path');
-// const fs = require('fs');
-// const webpack = require('webpack');
+const fs = require('fs');
+const webpack = require('webpack');
 const {
     override,
-    // fixBabelImports,
-    // addLessLoader,
-    // addWebpackAlias,
-    // addWebpackModuleRule,
-    // addWebpackPlugin,
+    fixBabelImports,
+    addLessLoader,
+    addWebpackAlias,
+    addWebpackModuleRule,
+    addWebpackPlugin,
 } = require('customize-cra');
-const rewireMultipleEntry = require('react-app-rewire-multiple-entry');
+// const rewireMultipleEntry = require('react-app-rewire-multiple-entry');
 
 function createEntry(name) {
     return {
-        // entry: `src/entries/${name}/index.js`,
-        entry: path.join(__dirname, `src/entries/${name}/index.js`),
+        entry: `src/entries/${name}/index.js`,
         template: 'public/index.html',
+        // entry: path.join(__dirname, `src/entries/${name}/index.js`),
+        // template: path.join(__dirname, `public/index.html`), 
         outPath: `/${name}.html`,
     };
 }
 
 const entries = ['home', 'login'].map(createEntry);
 console.log('entries:', entries);
-const multipleEntry = rewireMultipleEntry(entries);
+const multipleEntry = require('react-app-rewire-multiple-entry')(entries);
 
 const addEntry = () => config => {
     // config.optimization.splitChunks.name = process.env.NODE_ENV === 'development';
@@ -33,50 +34,57 @@ const addEntry = () => config => {
     return config;
 };
 
-module.exports = override(
-    addEntry(),
-    // fixBabelImports('import', {
-    //     libraryName: 'antd',
-    //     libraryDirectory: 'es',
-    //     style: true,
-    // }),
-    // addLessLoader({
-    //     javascriptEnabled: true,
-    //     modifyVars: {
-    //         '@primary-color': '#4780ff',
-    //         '@table-header-bg': '#f3f8ff',
-    //         '@table-row-hover-bg': '#f4f6f8',
-    //         '@border-radius-base': '2px',
-    //         '@table-padding-vertical': '10px',
-    //         '@table-selected-row-bg': '#dde5f0',
-    //     },
-    // }),
-    // addWebpackAlias({
-    //     '@': path.join(__dirname, './src'),
-    // }),
-    // addWebpackModuleRule({
-    //     test: /\.(png|svg|jpg|gif$)/,
-    //     use: [
-    //         {
-    //             loader: 'url-loader',
-    //             options: {
-    //                 limit: 15240,
-    //                 fallback: {
-    //                     loader: 'file-loader',
-    //                     options: {
-    //                         outputPath: 'static/img',
-    //                     },
-    //                 },
-    //             },
-    //         },
-    //     ],
-    // }),
-    // addWebpackPlugin(
-    //     new webpack.DefinePlugin({
-    //         'process.env': {
-    //             PKG_NAME: JSON.stringify(process.env.npm_package_name),
-    //             PKG_VERSION: JSON.stringify(process.env.npm_package_version),
-    //         },
-    //     })
-    // )
-);
+module.exports = {
+    webpack: override(
+        // multipleEntry.addMultiEntry
+        addEntry(),
+        fixBabelImports('import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+        }),
+        addLessLoader({
+            javascriptEnabled: true,
+            modifyVars: {
+                '@primary-color': '#4780ff',
+                '@table-header-bg': '#f3f8ff',
+                '@table-row-hover-bg': '#f4f6f8',
+                '@border-radius-base': '2px',
+                '@table-padding-vertical': '10px',
+                '@table-selected-row-bg': '#dde5f0',
+            },
+        }),
+        addWebpackAlias({
+            '@': path.join(__dirname, './src'),
+        }),
+        addWebpackModuleRule({
+            test: /\.(png|svg|jpg|gif$)/,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 15240,
+                        fallback: {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'static/img',
+                            },
+                        },
+                    },
+                },
+            ],
+        }),
+        // addWebpackPlugin(
+        //     new webpack.DefinePlugin({
+        //         'process.env': {
+        //             PKG_NAME: JSON.stringify(process.env.npm_package_name),
+        //             PKG_VERSION: JSON.stringify(process.env.npm_package_version),
+        //         },
+        //     })
+        // )
+    )
+    // webpack: function(config, env) {
+    //     multipleEntry.addMultiEntry(config);
+    //     return config;
+    // }
+}
---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      登陆页面1
+    </div>
+  );
+}
+
+export default App;
